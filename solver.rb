13ba803d4cb06325335db5bd1366e13b8885c774
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-05 14:17:51 -0600

    Fiz linter errors

--- solver.rb
+++ solver.rb
@@ -1,6 +1,6 @@
 class Solver
   def factorial(num)
-    if num <= 1 
+    if num <= 1
       1
     else
       num * factorial(num - 1)
@@ -18,15 +18,14 @@ class Solver
   end
 
   def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
+    if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif num % 3 == 0
+    elsif (num % 3).zero?
       'fizz'
-    elsif num % 5 == 0
+    elsif (num % 5).zero?
       'buzz'
     else
       num.to_s
     end
   end
-  
-end 
+end
